29c555508fe89523892f4f926b6e1ba8de77687b
andayani13 <89966738+andayani13@users.noreply.github.com>
2023-06-20 15:23:27 +0700

    perdana Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Assalaamu'alaikum"
+		response = "Assalaamu'alaikum wr wb"
 	}
 
 	fmt.Fprintln(w, response)
